0121c5fc525337e477efa2f19a9e2576b451ca8d
Juboraj Sujon <mdsujon72@gmail.com>
2024-03-26 02:12:56 +0600

    Navbar nav item styling done

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,55 +2,97 @@ import { Link, NavLink } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <div className="navbar font-work-sans px-4 fixed z-10">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor">
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
+    <div className="fixed top-0 w-full">
+      <div className="navbar container mx-auto  font-work-sans mt-4 px-4">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+              {/* navbar items for mobile device */}
+            </ul>
           </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-            {/* navbar items for mobile device */}
+          <a className="btn btn-ghost text-xl">Book Vibe</a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 space-x-1 *:text-lg font-semibold">
+            {/* navbar items for desktop device */}
+            <li className="*:hover:bg-green-400 hover:text-white">
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive
+                    ? "border border-clr-secondary text-clr-secondary hover:text-white"
+                    : ""
+                }>
+                Home
+              </NavLink>
+            </li>
+            <li className="*:hover:bg-green-400 hover:text-white">
+              <NavLink
+                to="/listedbooks"
+                className={({ isActive }) =>
+                  isActive
+                    ? "border border-clr-secondary text-clr-secondary hover:text-white"
+                    : ""
+                }>
+                Listed Books
+              </NavLink>
+            </li>
+            <li className="*:hover:bg-green-400 hover:text-white">
+              <NavLink
+                to="/pagestoread"
+                className={({ isActive }) =>
+                  isActive
+                    ? "border border-clr-secondary text-clr-secondary hover:text-white"
+                    : ""
+                }>
+                Pages to Read
+              </NavLink>
+            </li>
+            <li className="*:hover:bg-green-400 hover:text-white">
+              <NavLink
+                to="/feedback"
+                className={({ isActive }) =>
+                  isActive
+                    ? "border border-clr-secondary text-clr-secondary hover:text-white"
+                    : ""
+                }>
+                Feedback
+              </NavLink>
+            </li>
+            <li className="*:hover:bg-green-300 hover:text-white">
+              <NavLink
+                to="/donate"
+                className={({ isActive }) =>
+                  isActive
+                    ? "border border-clr-secondary text-clr-secondary hover:text-white"
+                    : ""
+                }>
+                Donate
+              </NavLink>
+            </li>
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">Book Vibe</a>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          {/* navbar items for desktop device */}
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/listedbooks">Listed Books</NavLink>
-          </li>
-          <li>
-            <NavLink to="/pagestoread">Pages to Read</NavLink>
-          </li>
-          <li>
-            <NavLink to="/feedback">Feedback</NavLink>
-          </li>
-          <li>
-            <NavLink to="/donate">Donate</NavLink>
-          </li>
-        </ul>
-      </div>
-      <div className="navbar-end">
-        <Link className="btn bg-clr-secondary">Sign In</Link>
-        <Link className="btn bg-clr-cyan ml-4">Sign Up</Link>
+        <div className="navbar-end">
+          <Link className="btn bg-clr-secondary">Sign In</Link>
+          <Link className="btn bg-clr-cyan ml-4">Sign Up</Link>
+        </div>
       </div>
     </div>
   );
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,26 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Nav from "../components/Nav";
-import Button from "../components/Button";
-import LinkComponent from "../components/LinkComponent";
-import LinkRoundFull from "../components/LinkRoundFull";
 
 const MainLayout = () => {
   return (
     <div>
-      <div className="h-20 py-4">
+      <div className="h-20">
         <Nav />
       </div>
-      <Button message="Donate" bgColor="bg-clr-cyan" textColor={"text-white"} />
-      <LinkComponent
-        message="Sign In"
-        bgColor="bg-clr-secondary"
-        textColor={"text-white"}
-      />
-      <LinkRoundFull
-        message="Sign Up"
-        bgColor="bg-clr-cyan"
-        textColor={"text-white"}
-      />
       <Outlet />
     </div>
   );
