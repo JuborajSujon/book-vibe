c4b2dd248532f2b24406fc9eee63c09f69ce0be6
Juboraj Sujon <mdsujon72@gmail.com>
2024-03-25 21:00:23 +0600

    Book vibe react project initialization

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <>
+      <h1 className="text-3xl font-bold underline">Book Vibe</h1>
+      <button className="btn btn-secondary">Secondary</button>
+    </>
+  </React.StrictMode>
+);
