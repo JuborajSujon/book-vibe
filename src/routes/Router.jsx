7baa45f28c767abe50da87085dcb6144fc0359c8
Juboraj Sujon <mdsujon72@gmail.com>
2024-03-25 22:40:53 +0600

    create our main layout and make all routing as children for mainlayout

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,24 +1,31 @@
 import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Home</div>,
-  },
-  {
-    path: "/about",
-    element: <div>Listed Books</div>,
-  },
-  {
-    path: "/contact",
-    element: <div>Pages to Read</div>,
-  },
-  {
-    path: "/feedback",
-    element: <div>Feedback</div>,
-  },
-  {
-    path: "/donate",
-    element: <div>Donate</div>,
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <div>Home</div>,
+      },
+      {
+        path: "/listedbooks",
+        element: <div>Listed Books</div>,
+      },
+      {
+        path: "/pagestoread",
+        element: <div>Pages to Read</div>,
+      },
+      {
+        path: "/feedback",
+        element: <div>Feedback</div>,
+      },
+      {
+        path: "/donate",
+        element: <div>Donate</div>,
+      },
+    ],
   },
 ]);
