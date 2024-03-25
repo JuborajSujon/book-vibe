c01ecfd4bb113414cce9fa4a503654169f9cb5c5
Juboraj Sujon <mdsujon72@gmail.com>
2024-03-25 21:15:41 +0600

    Google font added to project

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <>
-      <h1 className="text-3xl font-bold underline">Book Vibe</h1>
+      <h1 className="text-3xl font-bold underline font-playfair">Book Vibe</h1>
       <button className="btn btn-secondary">Secondary</button>
     </>
   </React.StrictMode>
